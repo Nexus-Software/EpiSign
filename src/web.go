dd116b29c21a429c08943520a4085faf4f730cb4
Enguerrand <contact@engue0774.fr>
2018-01-01 20:12:00 +0100

    addNewPage(feat): Add History and Settings page

--- src/web.go
+++ src/web.go
@@ -5,6 +5,7 @@ import (
 	"github.com/kataras/iris/context"
 )
 
+
 func main() {
 	app := iris.Default()
 
@@ -14,6 +15,14 @@ func main() {
 	// Resource: http://localhost:8080/
 	app.Handle("GET", "/", func(ctx context.Context) {
 		ctx.View("home.html")
+	})
+
+	app.Handle("GET", "/settings", func(ctx context.Context) {
+		ctx.View("settings.html")
+	})
+
+	app.Handle("GET", "/history", func(ctx context.Context) {
+		ctx.View("history.html")
 	})
 
 	// Set port
